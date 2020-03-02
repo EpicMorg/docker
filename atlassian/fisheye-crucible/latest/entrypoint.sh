66c1bfd9335b3423065d8907d01ca1acaeb2ba63
STAM <developer@epicm.org>
2020-03-02 13:18:13 +0300

    fishyey fix

--- atlassian/fisheye-crucible/latest/entrypoint.sh
+++ atlassian/fisheye-crucible/latest/entrypoint.sh
@@ -11,7 +11,7 @@ set -euo pipefail
 if [ "${JVM_MINIMUM_MEMORY}" != "" ]; then
     FISHEYE_OPTS="${FISHEYE_OPTS} -Xms${JVM_MINIMUM_MEMORY}"
 fi
-if [ "${JVM_MAXIMUM_MEMORY}" != "" ]; then	 
+if [ "${JVM_MAXIMUM_MEMORY}" != "" ]; then
     FISHEYE_OPTS="${FISHEYE_OPTS} -Xmx${JVM_MAXIMUM_MEMORY}"
 fi
 
@@ -20,14 +20,14 @@ export FISHEYE_OPTS="${FISHEYE_OPTS} ${JVM_SUPPORT_RECOMMENDED_ARGS}"
 # Start Bamboo as the correct user
 if [ "${UID}" -eq 0 ]; then
     echo "User is currently root. Will change directory ownership to ${RUN_USER}:${RUN_GROUP}, then downgrade permission to ${RUN_USER}"
-    PERMISSIONS_SIGNATURE=$(stat -c "%u:%U:%a" "${FISHEYE_HOME}")
+    PERMISSIONS_SIGNATURE=$(stat -c "%u:%U:%a" "${FISHEYE_INST}")
     EXPECTED_PERMISSIONS=$(id -u ${RUN_USER}):${RUN_USER}:700
     if [ "${PERMISSIONS_SIGNATURE}" != "${EXPECTED_PERMISSIONS}" ]; then
-        chmod -R 700 "${FISHEYE_HOME}" &&
-            chown -R "${RUN_USER}:${RUN_GROUP}" "${FISHEYE_HOME}"
+        chmod -R 700 "${FISHEYE_INST}" &&
+            chown -R "${RUN_USER}:${RUN_GROUP}" "${FISHEYE_INST}"
     fi
     # Now drop privileges
-    exec su -s /bin/bash "${RUN_USER}" -c "$FISHEYE_INST/bin/fisheyectl.sh $@"
+    exec su -s /bin/bash "${RUN_USER}" -c "$FISHEYE_HOME/bin/fisheyectl.sh $@"
 else
-    exec "$FISHEYE_INST/bin/fisheyectl.sh" "$@"
+    exec "$FISHEYE_HOME/bin/fisheyectl.sh" "$@"
 fi
