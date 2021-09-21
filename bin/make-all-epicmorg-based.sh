8e06cea0ddbf9aa029a640f685365767d12f343a
Odmin <admin@epicm.org>
2021-09-21 20:44:37 +0300

    folders

--- bin/make-all-epicmorg-based.sh
+++ bin/make-all-epicmorg-based.sh
@@ -2,60 +2,78 @@ export SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 clear
 
-cd ${SCRIPTPATH}/../linux/epicmorg/prod/main    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/prod/jdk6    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/prod/jdk7    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/prod/jdk8    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/prod/jdk11   && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/prod/main    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/prod/jdk6    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/prod/jdk7    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/prod/jdk8    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/prod/jdk11   && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/prod/jdk16   && pwd && make
 
-cd ${SCRIPTPATH}/../linux/epicmorg/edge/main    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/edge/jdk6    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/edge/jdk7    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/edge/jdk8    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/edge/jdk11   && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/edge/main    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/edge/jdk6    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/edge/jdk7    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/edge/jdk8    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/edge/jdk11   && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/edge/jdk16   && pwd && make
 
-cd ${SCRIPTPATH}/../linux/epicmorg/devel/main    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/devel/jdk6    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/devel/jdk7    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/devel/jdk8    && pwd && make
-cd ${SCRIPTPATH}/../linux/epicmorg/devel/jdk11   && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/devel/main    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/devel/jdk6    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/devel/jdk7    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/devel/jdk8    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/devel/jdk11   && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/epicmorg/devel/jdk16   && pwd && make
 
-cd ${SCRIPTPATH}/../linux/php/latest             && pwd && make
-cd ${SCRIPTPATH}/../linux/php/php7.2             && pwd && make
-cd ${SCRIPTPATH}/../linux/php/php7.3             && pwd && make
-cd ${SCRIPTPATH}/../linux/php/php7.4             && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/php/latest             && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/php/php7.2             && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/php/php7.3             && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/php/php7.4             && pwd && make
 
-cd ${SCRIPTPATH}/../linux/apache2/latest         && pwd && make
-cd ${SCRIPTPATH}/../linux/apache2/php7.2         && pwd && make
-cd ${SCRIPTPATH}/../linux/apache2/php7.3         && pwd && make
-cd ${SCRIPTPATH}/../linux/apache2/php7.4         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/apache2/latest         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/apache2/php7.2         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/apache2/php7.3         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/apache2/php7.4         && pwd && make
 
-cd ${SCRIPTPATH}/../linux/testrail/latest        && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/testrail/latest        && pwd && make
 
-cd ${SCRIPTPATH}/../linux/nginx/latest/main      && pwd && make
-cd ${SCRIPTPATH}/../linux/nginx/latest/php       && pwd && make
-cd ${SCRIPTPATH}/../linux/nginx/latest/rtmp-hls  && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/latest       && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/8.2          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/8.3          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/8.4          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/9.0          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/9.1          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/9.2          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/9.3          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/9.4          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/9.5          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/9.6          && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/10           && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/11           && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/12           && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/13           && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/postgres/14           && pwd && make
 
-cd ${SCRIPTPATH}/../linux/postgres/latest       && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/8.2          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/8.3          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/8.4          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/9.0          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/9.1          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/9.2          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/9.3          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/9.4          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/9.5          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/9.6          && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/10           && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/11           && pwd && make
-cd ${SCRIPTPATH}/../linux/postgres/12           && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/qbittorrent/latest    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/qbittorrent/stable    && pwd && make
 
-cd ${SCRIPTPATH}/../linux/qbittorrent/latest    && pwd && make
-cd ${SCRIPTPATH}/../linux/qbittorrent/stable    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/vk2discord/latest     && pwd && make
 
-cd ${SCRIPTPATH}/../linux/vk2discord/latest     && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/latest         && pwd && make
 
-cd ${SCRIPTPATH}/../linux/teamcity/agent        && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/amxx-sdk       && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/android-sdk    && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/atlassian-sdk  && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/dotnet-sdk     && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/node12         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/node14         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/node15         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/node16         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/php7.2         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/php7.3         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/php7.4         && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/teamcity/agent/steam-sdk      && pwd && make
+
+cd ${SCRIPTPATH}/../linux/ecosystem/nginx/latest/main      && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/nginx/latest/php       && pwd && make
+cd ${SCRIPTPATH}/../linux/ecosystem/nginx/latest/rtmp-hls  && pwd && make
 
 exit 0
--- bin/make-all-third-party.sh
+++ bin/make-all-third-party.sh
@@ -2,27 +2,27 @@ export SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 clear
 
-cd ${SCRIPTPATH}/../linux/zabbix/agent          && pwd && make
-cd ${SCRIPTPATH}/../linux/zabbix/java-gateway   && pwd && make
-cd ${SCRIPTPATH}/../linux/zabbix/proxy          && pwd && make
-cd ${SCRIPTPATH}/../linux/zabbix/server         && pwd && make
-cd ${SCRIPTPATH}/../linux/zabbix/web            && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/zabbix/agent          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/zabbix/java-gateway   && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/zabbix/proxy          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/zabbix/server         && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/zabbix/web            && pwd && make
 
 exit 1
 
-cd ${SCRIPTPATH}/../linux/mattermost/latest     && pwd && make
-cd ${SCRIPTPATH}/../linux/nextcloud/latest      && pwd && make
-cd ${SCRIPTPATH}/../linux/teamcity/server       && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/mattermost/latest     && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/latest      && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/teamcity/server       && pwd && make
 
 
-cd ${SCRIPTPATH}/../linux/nextcloud/14          && pwd && make
-cd ${SCRIPTPATH}/../linux/nextcloud/15          && pwd && make
-cd ${SCRIPTPATH}/../linux/nextcloud/16          && pwd && make
-cd ${SCRIPTPATH}/../linux/nextcloud/17          && pwd && make
-cd ${SCRIPTPATH}/../linux/nextcloud/18          && pwd && make
-cd ${SCRIPTPATH}/../linux/nextcloud/19          && pwd && make
-cd ${SCRIPTPATH}/../linux/nextcloud/20          && pwd && make
-cd ${SCRIPTPATH}/../linux/nextcloud/21          && pwd && make
-cd ${SCRIPTPATH}/../linux/nextcloud/22          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/14          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/15          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/16          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/17          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/18          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/19          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/20          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/21          && pwd && make
+cd ${SCRIPTPATH}/../linux/advanced/nextcloud/22          && pwd && make
 
 exit 0
