fda11ce9c19084adbde619151e34331703e879c5
Anatolii Zimovskii <azimovskii@saber3d.ru>
2021-12-16 19:11:50 +0300

    update

--- linux/ecosystem/apache2/php7.3/run.sh
+++ linux/ecosystem/apache2/php7.3/run.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
+echo "Changing permissions for /var/www path. Dont worry, please wait."
 chown www-data:www-data /var/www -R
+echo "Done. Starting up"
 source /etc/apache2/envvars
 tail -F /var/log/apache2/* &
 exec apache2 -D FOREGROUND
