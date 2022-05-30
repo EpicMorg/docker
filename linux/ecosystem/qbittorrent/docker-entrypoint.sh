01e13b422658e92b8c2884fad8f3f38f30dca798
STAM <stamepicmorg@users.noreply.github.com>
2022-05-30 16:15:57 +0300

    Update docker-entrypoint.sh

--- linux/ecosystem/qbittorrent/docker-entrypoint.sh
+++ linux/ecosystem/qbittorrent/docker-entrypoint.sh
@@ -2,8 +2,13 @@
 
 : ${QBT_PROFILES_DIR:=/opt/qbittorrent/profiles}
 : ${QBT_PROFILE_NAME:=docker}
+: ${QBT_PORT_WEBUI:=8282}
+: ${QBT_PORT_TRACKER:=9000}
 
-export LC_ALL=ru_RU.UTF-8
 
-echo "Starting qBitTorrent..."
-qbittorrent-nox --profile=${QBT_PROFILES_DIR} --configuration=${QBT_PROFILE_NAME} --webui-port=8282
+echo "======================================================"
+echo "[qbittorrent] Starting `qbittorrent-nox -v`..."
+echo "======================================================"
+
+tail -n 512 -f  ${QBT_PROFILES_DIR}/qBittorrent_${QBT_PROFILE_NAME}/data/logs/* &
+exec qbittorrent-nox -d --profile=${QBT_PROFILES_DIR} --configuration=${QBT_PROFILE_NAME} --webui-port=${QBT_PORT_WEBUI}
