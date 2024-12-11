ec2a1ea370a82dde4cb203697874d5b9f25ad0ef
STAM <stam@epicm.org>
2024-12-11 19:47:53 +0300

    web fixes

--- linux/ecosystem/apache2/php8.4/docker-entrypoint.sh
+++ linux/ecosystem/apache2/php8.4/docker-entrypoint.sh
@@ -1,18 +1,42 @@
 #!/bin/bash
 
-if [[ -z "${FIX_WWW_DATA}" ]]; then
-  echo "[apache2] env FIX_WWW_DATA is not set. Skipping..."
-elif [ "${FIX_WWW_DATA}" == "false" ]; then
-  echo "[apache2] env FIX_WWW_DATA is set to false. Skipping..."
-elif  [ "${FIX_WWW_DATA}" == "true" ]; then
-  echo "[apache2] Changing permissions for /var/www path. Dont worry, please wait."
-  chown www-data:www-data /var/www -R
-  echo "[apache2] Done"
-else
-  echo "[apache2] env FIX_WWW_DATA is set to strange value. Skipping..."
-fi
+set -e
 
 echo "[apache2] Starting up"
-source /etc/apache2/envvars
+
+case "${FIX_WWW_DATA}" in
+  "")
+    echo "[apache2] env FIX_WWW_DATA is not set. Skipping..."
+    ;;
+  "false")
+    echo "[apache2] env FIX_WWW_DATA is set to false. Skipping..."
+    ;;
+  "true")
+    echo "[apache2] Changing permissions for /var/www path. Please wait."
+    if [ -d "/var/www" ]; then
+      chown www-data:www-data /var/www -R
+      if [ $? -eq 0 ]; then
+        echo "[apache2] Permissions changed successfully."
+      else
+        echo "[apache2] Error changing permissions for /var/www."
+        exit 1
+      fi
+    else
+      echo "[apache2] /var/www directory not found. Skipping permission change."
+    fi
+    ;;
+  *)
+    echo "[apache2] env FIX_WWW_DATA is set to an invalid value. Skipping..."
+    ;;
+esac
+
+if [ -f /etc/apache2/envvars ]; then
+  source /etc/apache2/envvars
+else
+  echo "[apache2] Warning: /etc/apache2/envvars not found."
+fi
+
 tail -F /var/log/apache2/* &
+
+echo "[apache2] Starting Apache in the foreground."
 exec apache2 -D FOREGROUND
